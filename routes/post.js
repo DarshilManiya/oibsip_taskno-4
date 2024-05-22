20ebafcf8fd178daa2cdf2b20f9b3f45cf12f2d9
Darshil Maniya <darshil@Darshils-MacBook-Air.local>
2024-05-22 21:21:38 +0530

    committed

---
+++ routes/post.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/pinterest-data");
+
+const postSchema = new mongoose.Schema({
+  imgText: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: {
+    type: Array,
+    default: [],
+  },
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
