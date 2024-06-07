bf3bff0ff24ea6a74ac3d046d697fae76cf61cdb
Darshil Maniya <darshil@Darshils-MacBook-Air.local>
2024-06-07 19:43:53 +0530

    committed

--- app.js
+++ app.js
@@ -45,9 +45,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(flash());
 
-app.get("/home", (req, res) => {
-  res.render("home");
-});
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
